25a278472874145cd5fe031745faeaf6e5d439f4
Alex Chang <gom7745@gmail.com>
2015-04-26 18:13:48 +0800

    initial commit

---
+++ HackNTU/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HackNTU
+//
+//  Created by Alex on 2015/4/26.
+//  Copyright (c) 2015年 Alex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
